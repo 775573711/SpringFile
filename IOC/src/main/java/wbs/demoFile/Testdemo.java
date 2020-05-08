52dd111ba3a3947ef607b8d39d2196fb3706b597
wei <13696795141>
2020-05-08 15:23:42 +0800

    添加测试类Test

---
+++ IOC/src/main/java/wbs/demoFile/Testdemo.java
@@ -0,0 +1,31 @@
+package wbs.demoFile;
+
+public class Testdemo {
+    private Integer ID;
+    private String Name;
+    private String adder;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAdder() {
+        return adder;
+    }
+
+    public void setAdder(String adder) {
+        this.adder = adder;
+    }
+}
